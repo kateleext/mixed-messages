418ca236e87b55e77b4dc7390107217f390632ec
Kate Lee <kateleext@gmail.com>
2021-07-24 16:10:19 +0800

    version 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('this is my file')
+const starts = ['You must', 'You can', 'You must not', 'You will never']
+const verbs = ['eat', 'cook', 'order', 'demand']
+const foods = ['veggies', 'chilli', 'medicine', 'CBD', 'cake from Moono', 'OmniPork']
+
+function randMessage(first, second, third) {
+    firstIdx = Math.floor(Math.random() * first.length)
+    secondIdx = Math.floor(Math.random() * second.length)
+    thirdIdx = Math.floor(Math.random() * third.length)
+    return first[firstIdx] + ' ' + second[secondIdx] + ' ' + third[thirdIdx] + '.'
+}
+
+console.log(randMessage(starts, verbs, foods))
